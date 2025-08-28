913f3145c44adbb450ad91f29f0020f05ec7d1c8
Juliana <julianaakkemi@gmail.com>
2025-08-28 19:00:37 -0300

    add posts data model and repository pattern

---
+++ src/repositories/post/json-post-repository.ts
@@ -0,0 +1,32 @@
+import { PostModel } from "@/models/post/post-model";
+import { PostRepository } from "./post-repository";
+import { resolve } from "path";
+import { readFile } from "fs/promises";
+
+const ROOT_DIR = process.cwd();
+const JSON_POSTS_FILE_PATH = resolve(ROOT_DIR, "db", "seed", "posts.json"); //OS agnostic
+
+export class JsonPostRepository implements PostRepository {
+	private async readFromDisk(): Promise<PostModel[]> {
+		const jsonContent = await readFile(JSON_POSTS_FILE_PATH, "utf-8");
+		const parsedJson = JSON.parse(jsonContent);
+		const { posts } = parsedJson;
+		return posts;
+	}
+
+	async findAll(): Promise<PostModel[]> {
+		const posts = await this.readFromDisk();
+		return posts;
+	}
+
+	async finById(id: string): Promise<PostModel> {
+		const posts = await this.readFromDisk();
+		const post = posts.find((post) => post.id === id);
+
+		if (!post) {
+			throw new Error("Post not found");
+		}
+
+		return post;
+	}
+}
