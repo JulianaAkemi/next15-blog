3eae4f06d56308004b121def7af39bba4fb76f17
Juliana <julianaakkemi@gmail.com>
2025-08-29 14:36:40 -0300

    add Container and Post List components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
-export default function HomePage() {
-	return <div className="text-blue-500"></div>;
+import { Container } from "@/components/Container";
+import PostsList from "@/components/PostsList";
+import SpinLoader from "@/components/SpinLoader";
+import { Suspense } from "react";
+
+export default async function HomePage() {
+	return (
+		<Container>
+			<header>
+				<h1 className="text-6xl font-bold text-center py-8">HEADER</h1>
+			</header>
+
+			<Suspense fallback={<SpinLoader />}>
+				<PostsList />
+			</Suspense>
+
+			<footer>
+				<p className="text-6xl font-bold text-center py-8">Footer</p>
+			</footer>
+		</Container>
+	);
 }
