096337d96bd428e7d3dc94dfcae93eb44d6d09af
Juliana <julianaakkemi@gmail.com>
2025-08-29 19:22:55 -0300

    fix spin loader export and import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Container } from "@/components/Container";
 import { FeaturedPost } from "@/components/FeaturedPost";
 import { Header } from "@/components/Header";
 import { PostsList } from "@/components/PostsList";
-import SpinLoader from "@/components/SpinLoader";
+import { SpinLoader } from "@/components/SpinLoader";
 import { Suspense } from "react";
 
 export default async function HomePage() {
