404091c5669825abaf7acfd5bb88d5028869aed7
Juliana <julianaakkemi@gmail.com>
2025-08-29 19:48:13 -0300

    add date formatting

--- src/components/PostsList/index.tsx
+++ src/components/PostsList/index.tsx
@@ -1,7 +1,7 @@
 import { postRepository } from "@/repositories/post";
 import { PostCoverImage } from "../PostCoverImage";
 import { PostHeading } from "../PostHeading";
-import { formatDatetime } from "@/utils/format-datetime";
+import { formatDatetime, formatDistanceToNow } from "@/utils/format-datetime";
 
 export async function PostsList() {
 	const posts = await postRepository.findAll();
@@ -28,7 +28,7 @@ export async function PostsList() {
 						<div className="flex flex-col gap-4 sm:justify-center">
 							<time
 								className="text-slate-600 block text-sm/tight"
-								dateTime={post.createdAt}
+								title={formatDistanceToNow(post.createdAt)}
 							>
 								{formatDatetime(post.createdAt)}
 							</time>
