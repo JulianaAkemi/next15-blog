3163e6e42e6b1d83f38c05c57c672a5d15059eae
Juliana <julianaakkemi@gmail.com>
2025-08-29 21:19:42 -0300

    add layout use and pages metadata

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Sobre este blog",
+};
+
+export default function AboutPage() {
+	return <h1>Olá mundo</h1>;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
-import { Container } from "@/components/Container";
 import { FeaturedPost } from "@/components/FeaturedPost";
-import { Header } from "@/components/Header";
 import { PostsList } from "@/components/PostsList";
 import { SpinLoader } from "@/components/SpinLoader";
 import { Suspense } from "react";
 
 export default async function HomePage() {
 	return (
-		<Container>
-			<Header />
-
+		<>
 			<Suspense fallback={<SpinLoader />}>
 				<FeaturedPost />
 			</Suspense>
@@ -17,10 +13,6 @@ export default async function HomePage() {
 			<Suspense fallback={<SpinLoader />}>
 				<PostsList />
 			</Suspense>
-
-			<footer>
-				<p className="text-6xl font-bold text-center py-8">Footer</p>
-			</footer>
-		</Container>
+		</>
 	);
 }
