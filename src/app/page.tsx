7a28db98e08923d6941d2f440e9c4a8d9d419f71
Juliana <julianaakkemi@gmail.com>
2025-08-30 13:26:45 -0300

    add post date component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,9 @@ import { Suspense } from "react";
 export default async function HomePage() {
 	return (
 		<>
-			<Suspense fallback={<SpinLoader />}>
+			<Suspense fallback={<SpinLoader containerClasses="min-h-20 mb-16" />}>
 				<FeaturedPost />
-			</Suspense>
 
-			<Suspense fallback={<SpinLoader />}>
 				<PostsList />
 			</Suspense>
 		</>
