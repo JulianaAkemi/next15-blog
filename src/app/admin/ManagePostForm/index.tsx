331c09e8eba9e76f40728f474543bec7ebe5bb05
Juliana <julianaakkemi@gmail.com>
2025-09-13 19:06:09 -0300

    feat: add proper post creation form fields

--- src/app/admin/ManagePostForm/index.tsx
+++ src/app/admin/ManagePostForm/index.tsx
@@ -14,22 +14,56 @@ export function ManagePostForm() {
 		<form action="" className="mb-16">
 			<div className="flex flex-col gap-6">
 				<InputText
-					labelText="Nome"
-					placeholder="Digite seu nome"
-					type="password"
+					labelText="ID"
+					name="id"
+					placeholder="ID gerado automaticamente"
+					type="text"
+					defaultValue={""}
+					readOnly
 				/>
-				<InputText labelText="Sobrenome" placeholder="Digite seu sobrenome" />
-
-				<InputCheckbox labelText="Sobrenome" />
 
 				<InputText
-					disabled
-					labelText="Sobrenome"
-					placeholder="Digite seu sobrenome"
-					defaultValue="Olá mundo"
+					labelText="Slug"
+					name="slug"
+					placeholder="Slug gerada automaticamente"
+					type="text"
+					defaultValue={""}
+					readOnly
+				/>
+
+				<InputText
+					labelText="Autor"
+					name="author"
+					placeholder="Digite o nome do autor"
+					type="text"
+					defaultValue={""}
+				/>
+
+				<InputText
+					labelText="Título"
+					name="title"
+					placeholder="Digite o título"
+					type="text"
+					defaultValue={""}
+				/>
+
+				<InputText
+					labelText="Excerto"
+					name="excerpt"
+					placeholder="Digite o resumo"
+					type="text"
+					defaultValue={""}
 				/>
 
 				<ImageUploader />
+
+				<InputText
+					labelText="URL da imagem de capa"
+					name="coverImageUrl"
+					placeholder="Digite a url da imagem"
+					type="text"
+					defaultValue={""}
+				/>
 
 				<MarkdownEditor
 					labelText="Conteúdo"
@@ -39,12 +73,7 @@ export function ManagePostForm() {
 					setValue={setContentValue}
 				/>
 
-				<InputText
-					labelText="Sobrenome"
-					placeholder="Digite seu sobrenome"
-					defaultValue="Olá mundo"
-					readOnly
-				/>
+				<InputCheckbox labelText="Publicar?" name="published" type="checkbox" />
 
 				<div className="mt-4">
 					<Button type="submit">Enviar</Button>
--- src/app/admin/post/[id]/page.tsx
+++ src/app/admin/post/[id]/page.tsx
@@ -5,7 +5,7 @@ export const dynamic = "force-dynamic";
 export default async function AdminPostNewPage() {
 	return (
 		<>
-			<h1>Criar post</h1>
+			<h1 className="mb-5">Criar post</h1>
 
 			<ManagePostForm />
 		</>
