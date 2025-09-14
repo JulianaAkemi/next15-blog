050ddf646cfd8684191bc54c2bf866434053a275
Juliana <julianaakkemi@gmail.com>
2025-09-14 16:40:40 -0300

    chore: separate form to reuse on post edition and add DTO layer to post

--- src/app/admin/post/[id]/page.tsx
+++ src/app/admin/post/[id]/page.tsx
@@ -1,13 +1,36 @@
+import { makePublicPost } from "@/dto/post/dto";
+import { findPostByIdAdmin } from "@/lib/post/queries/admin";
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
 import { ManagePostForm } from "../../ManagePostForm";
 
 export const dynamic = "force-dynamic";
 
-export default async function AdminPostNewPage() {
+export const metadata: Metadata = {
+	title: "Editar post",
+};
+
+type AdminPostIdPageProps = {
+	params: Promise<{
+		id: string;
+	}>;
+};
+
+export default async function AdminPostIdPage({
+	params,
+}: AdminPostIdPageProps) {
+	const { id } = await params;
+	const post = await findPostByIdAdmin(id).catch();
+
+	if (!post) notFound();
+
+	const publicPost = makePublicPost(post);
+
 	return (
 		<>
-			<h1 className="mb-5">Criar post</h1>
+			<h1 className="text-xl font-extrabold md-5">Editar post</h1>
 
-			<ManagePostForm />
+			<ManagePostForm publicPost={publicPost} />
 		</>
 	);
 }
---
+++ src/app/admin/post/new/page.tsx
@@ -0,0 +1,18 @@
+import { Metadata } from "next";
+import { ManagePostForm } from "../../ManagePostForm";
+
+export const dynamic = "force-dynamic";
+
+export const metadata: Metadata = {
+	title: "Criar post",
+};
+
+export default async function AdminPostNewPage() {
+	return (
+		<>
+			<h1 className="text-xl font-extrabold mb-5">Criar post</h1>
+
+			<ManagePostForm />
+		</>
+	);
+}
