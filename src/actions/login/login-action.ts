9b9cdcee776fc1cfd6c7c1bf3ad7eb5ccede3f6d
Juliana <julianaakkemi@gmail.com>
2025-09-16 17:10:01 -0300

    build/feat: add JWT session and login

--- src/actions/login/login-action.ts
+++ src/actions/login/login-action.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { verifyPassword } from "@/lib/login/manage-login";
 import { asyncDelay } from "@/utils/async-delay";
 
 type LoginActionState = {
@@ -10,8 +11,39 @@ type LoginActionState = {
 export async function loginAction(state: LoginActionState, formData: FormData) {
 	await asyncDelay(5000);
 
+	if (!(formData instanceof FormData)) {
+		return {
+			username: "",
+			error: "Dados inválidos",
+		};
+	}
+
+	const username = formData.get("username")?.toString().trim() || "";
+	const password = formData.get("password")?.toString().trim() || "";
+
+	if (!username || !password) {
+		return {
+			username,
+			error: "Digite o usuário e a senha",
+		};
+	}
+
+	//TODO: check user on db
+	const isUsernameValid = username === process.env.LOGIN_USER;
+	const isPasswordValid = await verifyPassword(
+		password,
+		process.env.LOGIN_PASSWORD || ""
+	);
+
+	if (!isUsernameValid || !isPasswordValid) {
+		return {
+			username,
+			error: "Usuário ou senha inválidos",
+		};
+	}
+
 	return {
 		username: "",
-		error: "Teste de erro",
+		error: "USUÁRIO LOGADO COM SUCESSO!",
 	};
 }
